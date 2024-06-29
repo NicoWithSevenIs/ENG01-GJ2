b6badd3fd7fe565c22285dd8dd949d61f1c954f2
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-06-29 23:54:00 +0800

    Lamp

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    [SerializeField] private GameObject[] emittables;
+    [SerializeField] private Light l;
+
+    private bool _isTurnedOn = true;
+    public bool isTurnedOn { get { return _isTurnedOn; } set { _isTurnedOn = value; } }
+
+    private void Start()
+    {
+        flipSwitch();
+    }
+
+    public void flipSwitch()
+    {
+        isTurnedOn = !isTurnedOn;
+
+        l.enabled = isTurnedOn;
+
+        foreach(var emittable in emittables)
+        {
+            Material mat = emittable.GetComponent<MeshRenderer>().material;
+
+            if (isTurnedOn)
+                mat.EnableKeyword("_EMISSION");
+            else mat.DisableKeyword("_EMISSION");
+        }
+
+    }
+}
